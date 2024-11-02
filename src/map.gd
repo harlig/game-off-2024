dba48791dc0d46bbdabe16c4e409d08d2ff1517a
Christian Klug <cc1klug@hotmail.com>
2024-11-02 14:24:28 -0600

    Shrink the map

--- src/map.gd
+++ src/map.gd
@@ -5,16 +5,10 @@ class_name Map
 @onready var path_scene := preload("res://src/path.tscn");
 
 var initial_spawn_path_directions := 5
-var max_depth := 8
+var max_depth := 6
 var map_tree := {}
 var visited_nodes := []
 var available_nodes := []
-
-# I start in the center of the map
-# spawn $initial_spawn_path_directions paths in random directions
-# walk down each path and spawn a node each unit away, then another path and more nodes further down or stop
-# represent this as a strong data structure
-# keep track of nodes visited, and nodes available to visit based on where I am
 
 func _ready() -> void:
 	var center_node := Vector2(0, 0)
@@ -28,14 +22,17 @@ func generate_map(start_node: Vector2, directions: int, depth: int) -> void:
 	if depth >= max_depth:
 		return
 
-	for i in range(directions):
+	var attempts := 0
+	# ensure we retry when there are overlaps, so we create a good map size
+	while len(map_tree[start_node]) < directions and attempts < directions * 10:
+		attempts += 1
 		var direction := Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-		var new_node := start_node + direction * 100 # Scale the direction for better visibility
+		var new_node := start_node + direction
 
 		# Check for overlapping nodes
 		var overlap := false
 		for node: Vector2 in visited_nodes:
-			if node.distance_to(new_node) < 50: # Adjust the distance threshold as needed
+			if node.distance_to(new_node) < 1:
 				overlap = true
 				break
 
@@ -45,7 +42,7 @@ func generate_map(start_node: Vector2, directions: int, depth: int) -> void:
 			visited_nodes.append(new_node)
 			available_nodes.append(new_node)
 			# Recursively generate more paths from the new node
-			generate_map(new_node, directions - 1, depth + 1)
+			generate_map(new_node, directions, depth + 1)
 
 func visualize_map() -> void:
 	for parent_node: Vector2 in map_tree.keys():
@@ -53,12 +50,14 @@ func visualize_map() -> void:
 			# Create a StaticBody3D to represent the child node
 			var node := StaticBody3D.new()
 			node.position = Vector3(child_node.x, 1, child_node.y)
+			node.position.y = 1.2
 			add_child(node)
 
 			var node_sprite := Sprite3D.new()
 			node_sprite.texture = $Node.texture
 			node_sprite.rotation_degrees.x = -90
 			node.add_child(node_sprite)
+			node.scale = Vector3(0.05, 0.05, 0.05)
 
 			# Create a DottedLine to represent the connection
 			var path := path_scene.instantiate() as MeshInstance3D
@@ -75,6 +74,7 @@ func visualize_map() -> void:
 			var angle := acos(direction.normalized().dot(Vector3.RIGHT)) * sine
 			var length := direction.length() * 0.95
 			var new_pos := (start_pos + end_pos) / 2
+			new_pos.y = 1
 
 			add_child(path)
 			path.global_position = new_pos
