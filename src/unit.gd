67f1be2bfa688ca71bcd73b3337f1b82bb4c31db
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-01 19:51:46 -0700

    units stop each other

--- src/unit.gd
+++ src/unit.gd
@@ -5,11 +5,21 @@ enum Direction {LEFT, RIGHT}
 @export var direction: Direction = Direction.RIGHT
 var speed := 100
 
+var is_stopped := false
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	if is_stopped:
+		return
+
 	if direction == Direction.RIGHT:
 		position.x += speed * delta
 	else:
 		position.x -= speed * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Unit:
+		is_stopped = true
