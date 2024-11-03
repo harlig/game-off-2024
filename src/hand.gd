7885214124af3aa4806fe36d308f17ced81bd5ab
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-03 01:55:38 -0700

    draw cards on a timer

--- src/hand.gd
+++ src/hand.gd
@@ -8,6 +8,14 @@ var cards_in_hand: Array[Card] = []
 
 var combat_deck: CombatDeck
 
+# every 2.5 seconds draw a card
+func _ready() -> void:
+	var draw_timer := Timer.new()
+	draw_timer.wait_time = 2.5
+	draw_timer.connect("timeout", draw_and_deal)
+	add_child(draw_timer)
+	draw_timer.start()
+
 func setup_deck(deck: CombatDeck) -> void:
 	combat_deck = deck
 	deal_full_hand()
@@ -16,8 +24,15 @@ func deal_full_hand() -> void:
 	for ndx in range(hand_size):
 		deal_card(combat_deck.draw())
 
+func draw_and_deal() -> void:
+	deal_card(combat_deck.draw())
+
 func deal_card(card: Card) -> void:
 	print("Getting dealt card ", card)
+	if card == null:
+		print("No card to deal")
+		return
+
 	card.card_clicked.connect(_on_card_clicked)
 	add_child(card)
 	current_size += 1
@@ -33,6 +48,7 @@ func _on_card_clicked(times_clicked: int, card_instance: Card) -> void:
 		card_played.emit(card_instance)
 		remove_child(card_instance)
 		last_clicked_card = null
+		card_instance.disconnect("card_clicked", _on_card_clicked)
 		cards_in_hand.erase(card_instance)
 		combat_deck.discard(card_instance)
 
