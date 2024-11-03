6193eedaa56476ebb6de8294bbff28578f81b04e
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-03 14:14:15 -0800

    use combat nodes for sprite

--- src/map_node.gd
+++ src/map_node.gd
@@ -4,12 +4,18 @@ enum NodeType {
 	COMBAT
 }
 
+@onready var combat_node_sprite := preload("res://textures/combat_node.png")
+
 var node_type: NodeType = NodeType.COMBAT
 @onready var beat_node_sprite := preload("res://textures/check_box.png")
 
 var has_been_beaten := false
 
 signal node_clicked(node_position: Vector2)
+
+func _ready() -> void:
+	if node_type == NodeType.COMBAT:
+		$Sprite3D.texture = combat_node_sprite
 
 func _on_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if !event.is_pressed():
