eaf6c25ca311f0afa05a7ac7e552c8c084390b6c
jrockett6 <jr.rockett@gmail.com>
2024-11-04 14:11:43 -0800

    Fixup torches, add bushes

--- src/torch.gd
+++ src/torch.gd
@@ -4,3 +4,4 @@ func _ready() -> void:
     var seek_time := randf();
     $AnimationPlayer.seek(seek_time);
     print(seek_time);
+    $MeshInstance3D.material_override.set_shader_parameter("flip_h", seek_time > 0.5);
