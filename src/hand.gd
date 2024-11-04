00cc5c983b11de98ad6108e57e3b8779dfea3efe
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-03 21:19:22 -0800

    display mana

--- src/hand.gd
+++ src/hand.gd
@@ -1,7 +1,9 @@
 class_name Hand extends Control
 
 @onready var cards_area: HBoxContainer = $CardsArea
-@onready var mana_area: Control = $ManaArea
+@onready var mana_area_container: HBoxContainer = $ManaArea/HBoxContainer
+@onready var full_mana_icon := preload("res://textures/full_mana.png")
+@onready var empty_mana_icon := preload("res://textures/empty_mana.png")
 const HAND_SIZE := 5
 const MAX_MANA := 10
 
@@ -12,6 +14,26 @@ var cards_in_hand: Array[Card] = []
 var combat_deck: CombatDeck
 var mana_consumed := 0
 
+func _ready() -> void:
+	for ndx in range(MAX_MANA):
+		var icon := TextureRect.new()
+		icon.texture = full_mana_icon
+		icon.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
+		mana_area_container.add_child(icon)
+	replenish_mana()
+
+func replenish_mana() -> void:
+	mana_consumed = 0
+	for ndx in range(MAX_MANA):
+		var icon := mana_area_container.get_child(ndx) as TextureRect
+		icon.texture = full_mana_icon
+
+func use_mana(mana_used: int) -> void:
+	mana_consumed += mana_used
+	for ndx in range(mana_consumed):
+		var icon := mana_area_container.get_child(ndx) as TextureRect
+		icon.texture = empty_mana_icon
+
 func setup_deck(deck: CombatDeck) -> void:
 	combat_deck = deck
 	refresh_hand()
@@ -19,7 +41,7 @@ func setup_deck(deck: CombatDeck) -> void:
 func refresh_hand() -> void:
 	_discard_hand()
 	_deal_full_hand()
-	mana_consumed = 0
+	replenish_mana()
 
 func _deal_full_hand() -> void:
 	for ndx in range(HAND_SIZE):
@@ -56,7 +78,7 @@ func _on_card_clicked(times_clicked: int, card_instance: Card) -> void:
 
 
 func play_card(card: Card) -> void:
-	mana_consumed += card.data.mana
+	use_mana(card.data.mana)
 	card_played.emit(card)
 	discard(card)
 	cards_in_hand.erase(card)
