87905b295affc198e2d21dd358c753197636d800
jrockett6 <jr.rockett@gmail.com>
2024-11-05 20:26:19 -0800

    update card style

--- src/card.gd
+++ src/card.gd
@@ -1,4 +1,4 @@
-class_name Card extends AspectRatioContainer
+class_name Card extends TextureRect
 var is_selected := false
 var times_clicked := 0
 static var card_scene := preload("res://src/card.tscn")
@@ -24,15 +24,15 @@ var data: Data = Data.new() # Ensure data is instantiated
 signal card_clicked
 
 func _ready() -> void:
-	original_stylebox_override = $Background.get_theme_stylebox("panel")
+	original_stylebox_override = get_theme_stylebox("panel")
 	update_display()
 
 func update_display() -> void:
-	$Background/Title.text = data.card_name
-	$Background/Health.text = str(data.health)
-	$Background/Mana.text = str(data.mana)
-	$Background/Damage.text = str(data.damage)
-	$Background/TextureRect.texture = load(data.card_image_path)
+	$Title.text = data.card_name
+	$Health.text = str(data.health)
+	$Mana.text = str(data.mana)
+	$Damage.text = str(data.damage)
+	$TextureRect.texture = load(data.card_image_path)
 
 func set_stats(new_max_health: int, new_health: int, new_mana: int, new_damage: int, new_card_name: String, new_card_image_path: String, new_card_type: int) -> void:
 	data.max_health = new_max_health
@@ -54,7 +54,7 @@ func on_select() -> void:
 		card_clicked.emit(2, self)
 		style_box.bg_color = Color(120 / 255.0, 120 / 255.0, 120 / 255.0) # Red color; use RGBA values between 0-1
 
-	$Background.add_theme_stylebox_override("panel", style_box)
+	add_theme_stylebox_override("panel", style_box)
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -65,4 +65,4 @@ func _gui_input(event: InputEvent) -> void:
 func reset_selected() -> void:
 	is_selected = false
 	times_clicked = 0
-	$Background.add_theme_stylebox_override("panel", original_stylebox_override)
+	add_theme_stylebox_override("panel", original_stylebox_override)
