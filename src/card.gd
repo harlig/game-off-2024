0c66b23a186acfd7d375f426f2ce481138164d58
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-17 21:22:50 -0800

    use new card backs

--- src/card.gd
+++ src/card.gd
@@ -57,14 +57,20 @@ static func create_spell_card(init_spell: SpellList.Spell) -> Card:
 	return card_instance
 
 static func get_random_card_back() -> Texture:
+	# var card_backs := [
+	# 	"res://textures/card/back/card_back_1.png",
+	# 	"res://textures/card/back/card_back_2.png",
+	# 	"res://textures/card/back/card_back_3.png",
+	# 	"res://textures/card/back/card_back_4.png",
+	# 	"res://textures/card/back/card_back_5.png",
+	# 	"res://textures/card/back/card_back_6.png",
+	# 	"res://textures/card/back/card_back_7.png",
+	# ]
 	var card_backs := [
-		"res://textures/card/back/card_back_1.png",
-		"res://textures/card/back/card_back_2.png",
-		"res://textures/card/back/card_back_3.png",
-		"res://textures/card/back/card_back_4.png",
-		"res://textures/card/back/card_back_5.png",
-		"res://textures/card/back/card_back_6.png",
-		"res://textures/card/back/card_back_7.png",
+		"res://textures/card/back/torchless01.png",
+		"res://textures/card/back/torchless02.png",
+		"res://textures/card/back/torchless03.png",
+		"res://textures/card/back/torchless04.png",
 	]
 	return load(card_backs[randi() % card_backs.size()])
 ####################################################
