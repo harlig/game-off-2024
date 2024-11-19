13c6bbf59838439e92dd5a48723fda280af3d27c
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-18 22:53:07 -0800

    start to work on shop scene

--- src/between_combat.gd
+++ src/between_combat.gd
@@ -12,3 +12,16 @@ static func create_between_combat() -> BetweenCombat:
 
 func _on_button_pressed() -> void:
 	continue_pressed.emit()
+
+
+func _on_area_3d_mouse_entered() -> void:
+	$Area3D/MeshInstance3D.material_override.set_shader_parameter("highlight", true)
+
+
+func _on_area_3d_mouse_exited() -> void:
+	$Area3D/MeshInstance3D.material_override.set_shader_parameter("highlight", false)
+
+
+func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		print("Mouse clicked")
