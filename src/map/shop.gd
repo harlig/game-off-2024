55148a0f4333095ef314b7772357ef951e47248e
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-19 17:59:57 -0800

    use separate areas for units and spells

--- src/map/shop.gd
+++ src/map/shop.gd
@@ -1,6 +1,6 @@
 class_name Shop extends Control
 
-@onready var blank_card: Control = $OfferArea/BlankCard
+@onready var blank_card: Control = $BlankCard
 
 const SHOP_UNIT_COUNT := 4
 const SHOP_SPELL_COUNT := 4
@@ -43,14 +43,14 @@ func _ready() -> void:
 		new_card.name = "Unit {ndx}"
 		new_card.connect("card_clicked", _on_card_clicked)
 		units_in_shop.append(new_card)
-		$OfferArea.add_child(new_card)
+		$OfferArea/Units.add_child(new_card)
 
 	for ndx in range(SHOP_SPELL_COUNT):
 		var new_card := SpellList.new_card_by_id(randi() % SpellList.spell_cards.size())
 		new_card.name = "Spell {ndx}"
 		new_card.connect("card_clicked", _on_card_clicked)
 		spells_in_shop.append(new_card)
-		$OfferArea.add_child(new_card)
+		$OfferArea/Spells.add_child(new_card)
 
 
 func _on_card_clicked(times_clicked: int, card_instance: Card) -> void:
@@ -71,18 +71,21 @@ func _on_card_clicked(times_clicked: int, card_instance: Card) -> void:
 		var new_blank_card := blank_card.duplicate()
 		new_blank_card.show()
 		var index_of: int
+		var card_area: Control
 		match card_instance.type:
 			Card.CardType.UNIT:
 				index_of = units_in_shop.find(card_instance)
 				units_in_shop.remove_at(index_of)
+				card_area = $OfferArea/Units
 			Card.CardType.SPELL:
 				index_of = spells_in_shop.find(card_instance)
 				spells_in_shop.remove_at(index_of)
+				card_area = $OfferArea/Spells
 			_:
 				push_error("Unknown card type clicked in shop: ", card_instance.type)
-		$OfferArea.add_child(new_blank_card)
+		card_area.add_child(new_blank_card)
 		# no clue why I need the +1 here but it works
-		$OfferArea.move_child(new_blank_card, index_of + 1)
+		card_area.move_child(new_blank_card, index_of + 1)
 
 		card_instance.queue_free()
 		last_clicked_card = null
