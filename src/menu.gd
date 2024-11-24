0d337051ad43e7a96030d207edc691683f90cb13
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-23 18:40:47 -0800

    create tutorial combat

--- src/menu.gd
+++ src/menu.gd
@@ -1,6 +1,8 @@
 class_name Menu extends Control
 
 
+var tutorial_combat: Combat
+
 func _on_play_pressed() -> void:
 	var run: Run = load("res://src/run.tscn").instantiate()
 	add_sibling(run)
@@ -11,4 +13,16 @@ func _on_settings_pressed() -> void:
 	print("Settings")
 
 func _on_how_to_play_pressed() -> void:
+	# create combat, add it as a sibling, and remove the menu
+	# on combat end, it should remove itself and add the menu back
 	print("How to play")
+	var new_combat: Combat = Combat.create_combat(0, $Audio, [], 0, true)
+	tutorial_combat = new_combat
+	new_combat.combat_over.connect(_on_tutorial_combat_over)
+	add_sibling(new_combat)
+	hide()
+
+
+func _on_tutorial_combat_over() -> void:
+	show()
+	tutorial_combat.queue_free()
