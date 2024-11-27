ffb53a515c3e4a80b012ddc458e263e7b5a37da2
jrockett6 <jr.rockett@gmail.com>
2024-11-27 00:42:27 -0800

    moar grass

--- src/combat/backdrop.gd
+++ src/combat/backdrop.gd
@@ -2,13 +2,13 @@ extends Node3D
 
 func _ready() -> void:
 	# Grass
-	spawn_random(Vector2i(-30, -20), Vector2i(30, 20), Vector2i(40, 40), Vector2(1.0, 1.0), $GrassMM, 0.3)
+	spawn_random(Vector2i(-60, -40), Vector2i(60, 40), Vector2i(120, 80), Vector2(1.0, 1.0), $GrassMM, 0.3, 0.7, 0.5)
 
 	# Bush
 	spawn_random(Vector2i(-30, -15), Vector2i(30, 20), Vector2i(10, 10), Vector2(1.0, 1.0), $BushMM, 0.7)
 
 
-func spawn_random(from: Vector2i, to: Vector2i, partitions: Vector2i, overflow: Vector2, multimesh: MultiMeshInstance3D, scale_min: float) -> void:
+func spawn_random(from: Vector2i, to: Vector2i, partitions: Vector2i, overflow: Vector2, multimesh: MultiMeshInstance3D, scale_min: float, scale_max: float = 1.0, range_scale: float = 1.0) -> void:
 	var x_step := float(to.x - from.x) / float(partitions.x);
 	var y_step := float(to.y - from.y) / float(partitions.y);
 	var x_range := range(from.x, to.x, x_step);
@@ -20,7 +20,11 @@ func spawn_random(from: Vector2i, to: Vector2i, partitions: Vector2i, overflow: 
 	var ind := 0;
 
 	for x: int in x_range:
+		@warning_ignore("NARROWING_CONVERSION")
+		x *= range_scale;
 		for y: int in y_range:
+			@warning_ignore("NARROWING_CONVERSION")
+			y *= range_scale;
 			# var flip_x := float(randi() % 2 == 0);
 			var x_pos := randf_range(x - overflow.x, x + x_step + overflow.x);
 			var z_pos := randf_range(y - overflow.y, y + y_step + overflow.y);
@@ -33,7 +37,7 @@ func spawn_random(from: Vector2i, to: Vector2i, partitions: Vector2i, overflow: 
 			var xform := Transform3D();
 			xform.origin = Vector3(x_pos, y_pos, z_pos);
 			xform = xform.rotated_local(Vector3(1.0, 0.0, 0.0), -PI / 4)
-			xform = xform.scaled_local(Vector3.ONE * randf_range(scale_min, 1.0));
+			xform = xform.scaled_local(Vector3.ONE * randf_range(scale_min, scale_max));
 
 			multimesh.multimesh.set_instance_transform(ind, xform);
 			# multimesh.multimesh.set_instance_custom_data(ind, Color(x_pos, z_pos, 0.0, flip_x));
