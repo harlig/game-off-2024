ce47efd9f729edb86d0ce73b0d3ff7c8bf569dd1
Ethan Harlig <ethan.harlig@gmail.com>
2024-11-27 00:59:09 -0800

    cleanup

--- src/combat/backdrop.gd
+++ src/combat/backdrop.gd
@@ -9,37 +9,37 @@ func _ready() -> void:
 
 
 func spawn_random(from: Vector2i, to: Vector2i, partitions: Vector2i, overflow: Vector2, multimesh: MultiMeshInstance3D, scale_min: float, scale_max: float = 1.0, range_scale: float = 1.0) -> void:
-	var x_step := float(to.x - from.x) / float(partitions.x);
-	var y_step := float(to.y - from.y) / float(partitions.y);
-	var x_range := range(from.x, to.x, x_step);
-	var y_range := range(from.y, to.y, y_step);
+	var x_step := float(to.x - from.x) / float(partitions.x)
+	var y_step := float(to.y - from.y) / float(partitions.y)
+	var x_range := range(from.x, to.x, x_step)
+	var y_range := range(from.y, to.y, y_step)
 
 	# Throws Buffer argument error: https://github.com/godotengine/godot/issues/68592
 	multimesh.multimesh.instance_count = len(x_range) * len(y_range)
 
-	var ind := 0;
+	var ind := 0
 
 	for x: int in x_range:
 		@warning_ignore("NARROWING_CONVERSION")
-		x *= range_scale;
+		x *= range_scale
 		for y: int in y_range:
 			@warning_ignore("NARROWING_CONVERSION")
-			y *= range_scale;
-			# var flip_x := float(randi() % 2 == 0);
-			var x_pos := randf_range(x - overflow.x, x + x_step + overflow.x);
-			var z_pos := randf_range(y - overflow.y, y + y_step + overflow.y);
-			var y_pos := 0.0;
+			y *= range_scale
+			# var flip_x := float(randi() % 2 == 0)
+			var x_pos := randf_range(x - overflow.x, x + x_step + overflow.x)
+			var z_pos := randf_range(y - overflow.y, y + y_step + overflow.y)
+			var y_pos := 0.0
 
 			if z_pos > -13.0 and z_pos < -8.0:
-				y_pos = -10.0;
+				y_pos = -10.0
 
 
-			var xform := Transform3D();
-			xform.origin = Vector3(x_pos, y_pos, z_pos);
+			var xform := Transform3D()
+			xform.origin = Vector3(x_pos, y_pos, z_pos)
 			xform = xform.rotated_local(Vector3(1.0, 0.0, 0.0), -PI / 4)
-			xform = xform.scaled_local(Vector3.ONE * randf_range(scale_min, scale_max));
+			xform = xform.scaled_local(Vector3.ONE * randf_range(scale_min, scale_max))
 
-			multimesh.multimesh.set_instance_transform(ind, xform);
-			# multimesh.multimesh.set_instance_custom_data(ind, Color(x_pos, z_pos, 0.0, flip_x));
+			multimesh.multimesh.set_instance_transform(ind, xform)
+			# multimesh.multimesh.set_instance_custom_data(ind, Color(x_pos, z_pos, 0.0, flip_x))
 
-			ind += 1;
+			ind += 1
